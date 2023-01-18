c1b2e0e7704dc4d4f383f01faf9d03ac8e45a1f6
TpL <tranphilong030201@gmail.com>
2023-01-19 00:46:50 +0700

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,36 @@ function salary() {
     var spd = document.getElementById('spd').value * 1;
     var days = document.getElementById('days').value * 1;
     var salary = spd * days;
-    document.getElementById('rs').innerHTML = salary;
+    document.getElementById('rs-salary').innerHTML =
+        new Intl.NumberFormat().format(salary);
+}
+
+function average() {
+    var st1 = document.getElementById('sth1').value * 1;
+    var st2 = document.getElementById('sth2').value * 1;
+    var st3 = document.getElementById('sth3').value * 1;
+    var st4 = document.getElementById('sth4').value * 1;
+    var st5 = document.getElementById('sth5').value * 1;
+    document.getElementById('rs-average').innerHTML =
+        (st1 + st2 + st3 + st4 + st5) / 5;
+}
+
+function exchange() {
+    var usd = document.getElementById('usd').value;
+    document.getElementById('rs-exchange').innerHTML =
+        new Intl.NumberFormat().format(usd * 23500);
+}
+
+function area() {
+    var length = document.getElementById('length').value * 1;
+    var width = document.getElementById('width').value * 1;
+    document.getElementById('rs-area').innerHTML = length * width;
+    document.getElementById('rs-perimeter').innerHTML = (length + width) * 2;
+}
+
+function sum() {
+    var num = document.getElementById('num').value * 1;
+    var ones = num % 10;
+    var tens = Math.floor(num / 10);
+    document.getElementById('rs-sum').innerHTML = ones + tens;
 }
